fb7725da388751168a077ac32211b6d09d40c51e
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-08 15:37:05 -0500

    got it all done in nightmare mode.

--- bouncer.rb
+++ bouncer.rb
@@ -30,21 +30,22 @@ def check_age (age, country)
 end
 
 while true
-  puts "Are you from the UK or the US?"
-  country = gets.chomp.downcase
-  if (country == 'us') || (country== 'uk')
-    break
-  else
-    puts "Sorry, unfortunately this program only works for people from the UK or the US."
+  while true
+    puts "Are you from the UK or the US?"
+    country = gets.chomp.downcase
+    if (country == 'us') || (country== 'uk')
+      break
+    elsif country == 'quit'
+      break
+    else
+      puts "Sorry, unfortunately this program only works for people from the UK or the US."
+    end
   end
-end
-
-while true
   puts "How old are you?"
   age = gets.chomp
   if (age == 'quit') || (country == 'quit')
     break
-  elsif age.to_i < 1
+  elsif age.to_i <= 0
     puts "Please answer with a valid age."
   end
   check_age(age, country)
