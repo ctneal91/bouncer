1ae31f828f9b53c10312e29b84e9ea5f2d6e2c54
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-08 15:18:00 -0500

    it all works in nightmare mode, but it is not perfect...

--- bouncer.rb
+++ bouncer.rb
@@ -1,23 +1,51 @@
-def check_age (age)
-  if age.to_i >= 18
-    puts "You can vote."
-    puts "You can smoke."
-    if age.to_i >= 21
-      puts "You can drink."
-      if age.to_i >= 25
-        puts "You can rent a car."
+def check_age (age, country)
+    if (age.to_i >= 18)
+      puts "You can vote."
+      puts "You can smoke."
+      if country == 'uk'
+        puts "You can drink."
+      elsif age.to_i >= 21
+        puts "You can drink."
+        if age.to_i >= 25
+          puts "You can rent a car."
+        end
       end
+    elsif age.to_i >= 16 && country == 'uk'
+      while true
+        puts "Are your parents with you?"
+        answer = gets.chomp.downcase
+        if answer == 'yes'
+          puts "You can drink."
+          break
+        elsif answer == 'no'
+          puts "You can do nothing."
+          break
+        else
+          puts "Please answer yes or no."
+        end
+      end
+    elsif age.to_i > 0
+      puts "You can do nothing."
     end
-  elsif age.to_i > 0
-    puts "You are still a minor. Leave."
+end
+
+while true
+  puts "Are you from the UK or the US?"
+  country = gets.chomp.downcase
+  if (country == 'us') || (country== 'uk')
+    break
+  else
+    puts "Sorry, unfortunately this program only works for people from the UK or the US."
   end
 end
 
 while true
   puts "How old are you?"
   age = gets.chomp
-  check_age (age)
-  if age == 'quit'
+  if (age == 'quit') || (country == 'quit')
     break
+  elsif age.to_i < 1
+    puts "Please answer with a valid age."
   end
+  check_age(age, country)
 end
